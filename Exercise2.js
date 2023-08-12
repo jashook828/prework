f2879931a7539e9bf8d1f76e3e02881e4eb2a28a
Shook <63891580+jashook828@users.noreply.github.com>
2023-08-12 17:40:52 -0500

    Create Exercise2.js
    Exercise2 Complete

---
+++ Exercise2.js
@@ -0,0 +1,11 @@
+//This is exercise 2
+
+//console.log("Hello World!");
+
+/*
+console.log("There once was a writer of code");
+console.log('Who wanted to learn JS and node');
+console.log('They said "Once I can learn,"');
+console.log("'I'll increase what I earn.'");
+console.log("So now they're in full programmer mode!");
+*/
