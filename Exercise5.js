ba0d6b63bfc6e12320330f9cf393249ae6eb1bca
Shook <63891580+jashook828@users.noreply.github.com>
2023-08-18 19:52:48 -0500

    Create Exercise5.js

---
+++ Exercise5.js
@@ -0,0 +1,13 @@
+// This is exercise 5
+
+/*console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+*/
+console.log(5.0 == 5); // ==, true
+console.log(6 > 100); // >, false
+console.log(6 >= 7); // <=, false
+console.log(7 <= 7); // !=, true
