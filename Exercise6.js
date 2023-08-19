87f9dab2db267e6a06a326b32c65a5a2eb105d89
Shook <63891580+jashook828@users.noreply.github.com>
2023-08-18 20:19:55 -0500

    Create Exercise6.js

---
+++ Exercise6.js
@@ -0,0 +1,25 @@
+// This is exercise 6
+
+/*console.log("true or false: wrapping in double quotes is the same as wrapping in single quotes");
+console.log("Hello world!" == 'Hello world!');
+
+console.log("true or false: using single quotes inside a string is the same as using double quotes inside the string");
+console.log("He said 'no'!" == 'He said "no"!');
+
+console.log("true or false: strings that say the same thing but have different capitalization are not equal");
+console.log("Hello world!" != "hello world!");
+
+console.log("true or false: in javascript, a string is considered less than another string if it would come before it in alphabetical order");
+console.log("apple" < "banana");
+
+console.log("The number 5 is the same value as the string '5'");
+console.log(5 == "5");
+
+console.log("The number 5 is the same value and same data type as the string '5'");
+console.log(5 === "5"); */
+
+console.log('1' == 1); // ==, true
+console.log(6 < '5'); // <, false
+console.log("100" !== "100"); // !==, false
+console.log(100 != '95'); // !=, true
+console.log("100" === '100'); // ===, true
